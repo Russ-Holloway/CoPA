5df7c9a2f690ad5d37631e546febff39bfc3b837
Russ Holloway <russell.holloway@btp.police.uk>
2025-06-23 09:05:07 +0100

    Delete update_deploy_template.cmd

--- update_deploy_template.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Updating deployment templates and generating deployment URL...
-powershell -ExecutionPolicy Bypass -File "%~dp0update_deploy_template.ps1"
-echo.
-echo If the script completed successfully, you'll find your deployment URL in azure_deploy_url.txt
-pause
