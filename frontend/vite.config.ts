70895c47b3b15d745a9fb99d51beccc860d1448b
Russ Holloway <russell.holloway@btp.police.uk>
2025-07-27 12:21:37 +0000

    updated layout for clarity

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,12 +11,12 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      '/ask': 'http://localhost:50505',
-      '/chat': 'http://localhost:50505',
-      '/conversation': 'http://localhost:50505',
-      '/history': 'http://localhost:50505',
-      '/.auth': 'http://localhost:50505',
-      '/frontend_settings': 'http://localhost:50505'
+      '/ask': 'http://localhost:8001',
+      '/chat': 'http://localhost:8001',
+      '/conversation': 'http://localhost:8001',
+      '/history': 'http://localhost:8001',
+      '/.auth': 'http://localhost:8001',
+      '/frontend_settings': 'http://localhost:8001'
     }
   }
 })
